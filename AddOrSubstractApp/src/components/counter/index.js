dd42397413434c5ef5d0244d9c5e3000624bafb9
NicolasBNZ <nicolas.benzadon@gmail.com>
2022-03-30 13:07:11 +0200

    :lipstick: style done

--- AddOrSubstractApp/src/components/counter/index.js
+++ AddOrSubstractApp/src/components/counter/index.js
@@ -5,12 +5,18 @@ import "./index.css"
 export default function Counter() {
 const [count, setCount] = useState(0);
 
-    return (
-      <div className="counter">
-          <h1 className="result" data-testid="value">{count}</h1>
-          <button className="increment" onClick={() => setCount(count +1 )}>+</button>
-          <button className="decrement" onClick={() => setCount(count -1 )}>-</button>
+return (
+<div className='fullcontainer'>
+  <div className="counter">
+      <h1 className="result" data-testid="value">{count}</h1>
+      <div className='buttonscontainer'>
+        <button className="increment" onClick={() => setCount(count +1 )}>+</button>
+        <button className="decrement" onClick={() => setCount(count -1 )}>-</button>
       </div>
+
+  </div>
+</div>
+    
       
     );
   }
